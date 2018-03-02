4d198be0e1900529c759a144945fc26965934a6f
Andrew Johnson <32281250+andrewj12763@users.noreply.github.com>
2018-03-01 18:12:01 -0700

    finishing html and adding some JS

--- logic.js
+++ logic.js
@@ -1,107 +1,82 @@
-/* global moment firebase */
+$(function(){
 
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyCUa3OmzBQAV9MHxQg6Pgl2s5533V5qjEI",
-  authDomain: "coder-bay-fee9d.firebaseapp.com",
-  databaseURL: "https://coder-bay-fee9d.firebaseio.com",
-  storageBucket: "coder-bay-fee9d.appspot.com"
-};
+  // Initialize Firebase
+	var config = {
+	    apiKey: "AIzaSyDXLNdTzO7HXLy17LsC-9x45Er_hyh80S8",
+	    authDomain: "train-schedule-e4574.firebaseapp.com",
+	    databaseURL: "https://train-schedule-e4574.firebaseio.com",
+	    projectId: "train-schedule-e4574",
+	    storageBucket: "train-schedule-e4574.appspot.com",
+	    messagingSenderId: "1064809053893"
+	};
 
-firebase.initializeApp(config);
+  	firebase.initializeApp(config);
 
-// Create a variable to reference the database
-var database = firebase.database();
+  	var trainData = firebase.database();
 
-// Initial Values
-var initialBid = 0;
-var initialBidder = "No one :-(";
-var highPrice = initialBid;
-var highBidder = initialBidder;
+  	$('#newTrain').click(function(){
+		var trainName = $('#trainName-input').val().trim();
+		var destination = $('#destination-input').val().trim();
+		var startTime = moment($('#startTime-input').val().trim(), 'HH:mm').subtract(10,'years').format('X');
+		var frequency = $('#frequency-input').val();
 
-// --------------------------------------------------------------
+			
 
-// At the initial load and subsequent value changes, get a snapshot of the stored data.
-// This function allows you to update your page in real-time when the firebase database changes.
-database.ref().on("value", function(snapshot) {
+		var newTrain = {
+			name: trainName,
+			destination: destination,
+			startTime: startTime,
+			frequency: frequency
+		}
 
-  // If Firebase has a highPrice and highBidder stored (first case)
-  if (snapshot.child("highBidder").exists() && snapshot.child("highPrice").exists()) {
-    // Set the variables for highBidder/highPrice equal to the stored values.
-    highBidder = snapshot.val().highBidder;
-    highPrice = parseInt(snapshot.val().highPrice);
+		trainData.ref().push(newTrain);
 
-    // Change the text inside the HTML element to reflect the initial value
-    $("#highest-bidder").text(snapshot.val().highBidder);
-    $("#highest-price").text("$" + snapshot.val().highPrice);
+			alert('Train Added!!!');
 
-    // Print the data to the console.
-    console.log(snapshot.val().highBidder);
-    console.log(snapshot.val().highPrice);
-  }
+			$('#trainName-input').val("");
+			$('#destination-input').val("");
+			$('#startTime-input').val("");
+			$('#frequency-input').val("");
+	})
 
-  // Keep the variables for highBidder/highPrice equal to the initial values
-  else {
+		trainData.ref().on('child_added', function(snapshot){
+			var trainName = snapshot.val().name; 
+			var destination = snapshot.val().destination; 
+			var frequency = snapshot.val().frequency; 
+			var startTime = snapshot.val().startTime; 
 
-    // Change the HTML to reflect the initial value
-    $("#highest-bidder").text(highBidder);
-    $("#highest-price").text("$" + highPrice);
+			var remainder = moment().diff(moment.unix(startTime),"minutes")%frequency;
+			var minutes = frequency - remainder;
+			var arrival = moment().add(minutes,'m').format('hh:mm A');
 
-    // Print the initial data to the console.
-    console.log("Current High Price");
-    console.log(highBidder);
-    console.log(highPrice);
-  }
+			console.log('Start Time: ', startTime);
+			console.log('Remainder: ', remainder);
+			console.log('Minutes: ', minutes);
+			console.log('Arrival: ',arrival);
 
-  // If any errors are experienced, log them to console.
-}, function(errorObject) {
-  console.log("The read failed: " + errorObject.code);
-});
+			$('#schedule').append(`<tr><td>${trainName}</td><td>${destination}</td><td>${frequency}</td><td>${arrival}</td><td>${minutes}</td>`)
+		})	
+// Click '+' to Open Add Train and "x" to Close window
 
-// --------------------------------------------------------------
+	$('#add').click(function(){
+		if($('#newTrainSchedule').attr('data-status') === 'hide') {
+			$('#newTrainSchedule').attr('data-status', 'show').css({'visibility': 'visible', 'height': '480px'});
+			$('#symbol').removeClass('fa fa-plus').addClass('fa fa-close');
+		} else {
+			$('#newTrainSchedule').attr('data-status', 'hide').css({'visibility': 'hidden', 'height': '0px'});
+			$('#symbol').removeClass('fa fa-close').addClass('fa fa-plus');
+		}
+	});
+// Clock showing on the right side of schedule 
+	function currentTime() {
+		var sec = 1;	
+		time = moment().format('HH:mm:ss');
+		searchTime = moment().format('HH:mm');
+			$('#currentTime').html(time);
 
-// Whenever a user clicks the submit-bid
-
-$("#submit-bid").on("click", function() {
-  // Get the input values
-  var bidderName = $("#bidder-name").val().trim();
-  var bidderPrice = parseInt($("#bidder-price").val().trim());
-
-  // Log the Bidder and Price (Even if not the highest)
-  console.log(bidderName);
-  console.log(bidderPrice);
-
-  if (bidderPrice > highPrice) {
-
-    // Alert
-    alert("You are now the highest bidder.");
-
-    // Save the new price in Firebase
-    database.ref().set({
-      highBidder: bidderName,
-      highPrice: bidderPrice
-    });
-
-    // Log the new High Price
-    console.log("New High Price!");
-    console.log(bidderName);
-    console.log(bidderPrice);
-
-    // Store the new high price and bidder name as a local variable (could have also used the Firebase variable)
-    highBidder = bidderName;
-    highPrice = parseInt(bidderPrice);
-
-    // Change the HTML to reflect the new high price and bidder
-    $("#highest-bidder").text(bidderName);
-    $("#highest-price").text("$" + bidderPrice);
-  }
-
-  else {
-
-    // Alert
-    alert("Sorry that bid is too low. Try again.");
-  }
-
-  // Prevent default behavior
-  event.preventDefault();
+			t = setTimeout(function() {
+				currentTime();
+			}, sec * 1000);	
+	}
+	currentTime(); 
 });
