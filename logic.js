f77c46d1eb8daecbb720813489dd9b593fa08dc1
Andrew Johnson <32281250+andrewj12763@users.noreply.github.com>
2018-03-02 13:42:09 -0700

    adding functionality

--- logic.js
+++ logic.js
@@ -1,82 +1,45 @@
-$(function(){
+var config = {
+	apiKey: "AIzaSyAiXwMPzJfs5tsIfSqgoROoHfCpdXaP-HE",
+	authDomain: "train-638c4.firebaseapp.com",
+	databaseURL: "https://train-638c4.firebaseio.com",
+	projectId: "train-638c4",
+	storageBucket: "train-638c4.appspot.com",
+	messagingSenderId: "537339644224"
+};
 
-  // Initialize Firebase
-	var config = {
-	    apiKey: "AIzaSyDXLNdTzO7HXLy17LsC-9x45Er_hyh80S8",
-	    authDomain: "train-schedule-e4574.firebaseapp.com",
-	    databaseURL: "https://train-schedule-e4574.firebaseio.com",
-	    projectId: "train-schedule-e4574",
-	    storageBucket: "train-schedule-e4574.appspot.com",
-	    messagingSenderId: "1064809053893"
-	};
+firebase.initializeApp(config);
 
-  	firebase.initializeApp(config);
+var database = firebase.database();
 
-  	var trainData = firebase.database();
+$("#btn").on('click', function() {
+	event.preventDefault();
+	var trainName = $("#trainName").val();
+	var destination = $("#destination").val();
+	var frequency = $("#frequency").val();
+	var nextArrival = $("#nextArrival").val();;
+	console.log("variable output", name);
+	database.ref().push({
+		trainName: trainName,
+		destination: destination,
+		frequency: frequency,
+		nextArrival: nextArrival,
+		dataAdded: firebase.database.ServerValue.TIMESTAMP
 
-  	$('#newTrain').click(function(){
-		var trainName = $('#trainName-input').val().trim();
-		var destination = $('#destination-input').val().trim();
-		var startTime = moment($('#startTime-input').val().trim(), 'HH:mm').subtract(10,'years').format('X');
-		var frequency = $('#frequency-input').val();
-
-			
-
-		var newTrain = {
-			name: trainName,
-			destination: destination,
-			startTime: startTime,
-			frequency: frequency
-		}
-
-		trainData.ref().push(newTrain);
-
-			alert('Train Added!!!');
-
-			$('#trainName-input').val("");
-			$('#destination-input').val("");
-			$('#startTime-input').val("");
-			$('#frequency-input').val("");
-	})
-
-		trainData.ref().on('child_added', function(snapshot){
-			var trainName = snapshot.val().name; 
-			var destination = snapshot.val().destination; 
-			var frequency = snapshot.val().frequency; 
-			var startTime = snapshot.val().startTime; 
-
-			var remainder = moment().diff(moment.unix(startTime),"minutes")%frequency;
-			var minutes = frequency - remainder;
-			var arrival = moment().add(minutes,'m').format('hh:mm A');
-
-			console.log('Start Time: ', startTime);
-			console.log('Remainder: ', remainder);
-			console.log('Minutes: ', minutes);
-			console.log('Arrival: ',arrival);
-
-			$('#schedule').append(`<tr><td>${trainName}</td><td>${destination}</td><td>${frequency}</td><td>${arrival}</td><td>${minutes}</td>`)
-		})	
-// Click '+' to Open Add Train and "x" to Close window
-
-	$('#add').click(function(){
-		if($('#newTrainSchedule').attr('data-status') === 'hide') {
-			$('#newTrainSchedule').attr('data-status', 'show').css({'visibility': 'visible', 'height': '480px'});
-			$('#symbol').removeClass('fa fa-plus').addClass('fa fa-close');
-		} else {
-			$('#newTrainSchedule').attr('data-status', 'hide').css({'visibility': 'hidden', 'height': '0px'});
-			$('#symbol').removeClass('fa fa-close').addClass('fa fa-plus');
-		}
 	});
-// Clock showing on the right side of schedule 
-	function currentTime() {
-		var sec = 1;	
-		time = moment().format('HH:mm:ss');
-		searchTime = moment().format('HH:mm');
-			$('#currentTime').html(time);
 
-			t = setTimeout(function() {
-				currentTime();
-			}, sec * 1000);	
-	}
-	currentTime(); 
+	console.log(name);
+
 });
+
+
+
+database.ref().on("child_added", function (childSnapshot) {
+	console.log('childSnap: ', childSnapshot.val());
+	var val = childSnapshot.val();
+	$("#employeeInfo").append("<tr><td class='trainName'>" + val.trainName + "</td><td class='destination'>" + val.destination + "</td>" + "</td><td class='frequency'>" + val.frequency + "</td>" + "<td class='nextArrival'>" + val.nextArrival + "</td><td class='minAway'>" + Math.abs(val.currentTime.dif()) + "</td></tr>");
+});
+
+var currentTime = moment();
+// var nextArrivalTime = (nextArrival
+var nextTrain = currentTime.add(frequency, 'minutes').format('HH:mm');
+console.log("arrival time", nextTrain);
